754ccb9ed81208d193f51b77a11a07f7a032903f
Marios Avgeris <avgeris.marios@gmail.com>
2018-09-20 02:06:30 +0300

    introduce main menu page

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:starter_app/pages/main_menu_page.dart';
 import 'package:starter_app/pages/object_list_page.dart';
 import 'capture_page.dart';
 import '../utils/camera.dart';
@@ -11,7 +12,8 @@ class LandingPage extends StatelessWidget {
       child: new InkWell( // invisible button that registers clicks or taps
         // praktika einai to stack to opoio kanei push kai pop full pages kathws kaneis navigate. 
 //        onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new CameraHome())),
-        onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new ObjectListPage())),
+//        onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new ObjectListPage())),
+        onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new MainMenuPage())),
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center, // Y Axis Alignment
           children: <Widget>[
---
+++ lib/pages/main_menu_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:starter_app/pages/object_list_page.dart';
+import 'capture_page.dart';
+import '../utils/camera.dart';
+
+class MainMenuPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Material (
+      color: Colors.blueAccent,
+      child: new InkWell( // invisible button that registers clicks or taps
+        // praktika einai to stack to opoio kanei push kai pop full pages kathws kaneis navigate.
+//        onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new CameraHome())),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Y Axis Alignment
+          children: <Widget>[
+            new FlatButton(
+//                onPressed: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new ObjectListPage())),
+                child: new Text("Battle Ground", style: new TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold))
+            ),
+            new FlatButton(
+                onPressed: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new ObjectListPage())),
+                child: new Text("Single Player", style: new TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold))
+            ),
+            new FlatButton(
+//                onPressed: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new ObjectListPage())),
+                child: new Text("Settings", style: new TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold))
+            ),
+            new FlatButton(
+//                onPressed: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new ObjectListPage())),
+                child: new Text("Profile", style: new TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold))
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
